997d6f3a9074e3a8638fce952cdc4a43f11781de
roger-tb <60202408+roger-tb@users.noreply.github.com>
2020-01-23 10:17:05 +1100

    First commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import './App.css';
+import Login from "./pages/login/index";
+import styled from 'styled-components';
+import CustomChatbot from "./pages/chatbot/index"
+class App extends Component {
+  
+  
+  
+  render() {
+    return (
+      <Router>
+        <div className="App">
+        <Styledheader>Terobot Authentication</Styledheader>
+          
+          <Route path="/login" component={Login} />
+          <Route path="/chatbot" component={CustomChatbot} />
+         
+        </div>
+      </Router>
+     
+    );
+  }
+}
+ const Styledheader = styled.header`
+ border-bottom: 1px solid #777;
+ position: sticky;
+ top: 0;
+ zIndex: 9000;
+ height: 62px;
+ maxHeight: 62px;
+ `;
+export default App;
